b9e41b68505c9545a72d74866baf44e5865ddc09
NikiiG <nikii1405abraham@gmail.com>
2023-07-10 20:21:53 -0700

    create Flights index

--- routes/flights.js
+++ routes/flights.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// You'll be creating this controller module next
+const flightsCtrl = require('../controllers/flights');
+
+router.get('/', flightsCtrl.index);
+
+router.get('/new', flightsCtrl.new);
+// POST /flights
+router.post('/', flightsCtrl.create);
 
 module.exports = router;
